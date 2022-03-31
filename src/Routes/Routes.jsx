16fee1d8bae9f08182a1e59cadfdb296af23a5bf
Akash Borate <27akashborate@gmail.com>
2022-03-31 13:05:42 +0530

    commit

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes } from "react-router-dom";
 import { Footer } from "../Components/Footer/Footer";
 import { Home } from "../Components/Home/Home";
 
