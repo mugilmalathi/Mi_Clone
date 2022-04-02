d211bc2377d00fd19253353d6cc2c6de865b19a8
Mugilarasan <95951189+mugilmalathi@users.noreply.github.com>
2022-04-02 18:52:25 +0530

    product features page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { ProductFeatures } from './ProductFeatures/ProductFeatures';
+
+function App() {
+  return (
+    <div className="App">
+      <ProductFeatures />
+    </div>
+  );
+}
+
+export default App;
